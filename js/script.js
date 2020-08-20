605315089a1acffaffde34580924ed88409b37d7
TySod <oyeniyi4real@gmail.com>
2020-08-20 21:14:45 +0100

    trying to test the submit feature

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#messageForm');
+const message_display = document.querySelector('#messages');
+const message = document.querySelector('#message-input')
+
+console.log(message_display)
+console.log(message)
+console.log(form)  
+
+const chatDisplay = ()=>{
+  form.preventDefault();
+  const li = document.createElement("li")
+  li.textContent = messsage.value
+  li.classList.add('msg my')
+  message_display.appendChild(li)
+  console.log(message_display)
+  console.log(message)
+  console.log(form)  
+  console.log(li)  
+}
+
+form.addEventListener("onsubmit", chatDisplay)
